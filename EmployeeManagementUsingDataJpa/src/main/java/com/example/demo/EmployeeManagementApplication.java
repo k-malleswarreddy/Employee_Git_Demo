d66e3519c0d19b35cca31521bf1820263e588a99
mallesh <malleswar863@gmail.com>
2026-03-13 14:41:03 +0530

    Second commit from sts

--- EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -8,6 +8,7 @@ public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("First line in STS after git");
+		System.out.println("second");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
